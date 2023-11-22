4732182c1738731fb481dd8d2e131d367125415c
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-22 13:22:30 +0100

    books models

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Local
+    "books.apps.BooksConfig" # new 
 ]
 
 ROOT_URLCONF = 'django_project.urls'
